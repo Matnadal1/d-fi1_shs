172f9023b51558745b361eecd5bbcfd2fcab9cd1
Matnadal1 <matnadal.star2@gmail.com>
2023-02-08 12:10:07 +0100

    J'ai simplifié le code javascript et factoriser (création de fonction pour chaques fonctions qui s'appelent entre elles)

--- scripts/script_index.js
+++ scripts/script_index.js
@@ -1,117 +1,82 @@
+// Récupère et affiche les lignes quand on appuis sur envoyer
+
 document.querySelector("#button1").addEventListener("click", function(){
-    const xhr = new XMLHttpRequest();
-    xhr.open("get", "https://api.tisseo.fr/v2/lines.json?key=a3732a1074e2403ce364ad6e71eb998cb")
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            let lines = JSON.parse(xhr.responseText)
-            let lignes = lines.lines.line
+    fetchLines();
+});
 
-            // On supprime les éléments html affichés lors de la derniere requete
-
-            document.querySelector("#liste").innerHTML = "" 
-
-            // On trie les lignes dans l'ordre croissant de leur id
-
-            lignes.sort(function(a, b) {
-                return parseInt(a.id.split(':')[1]) - parseInt(b.id.split(':')[1]);
-            });
-            
-            // On affiche les lignes, ligne par ligne
-            
-            for(let line of lignes) {
-                let id = line.id;
-                id = id.replace("line:", "");
-                document.querySelector("#liste").innerHTML+="<li class='ligne' id=\""+line.id+"\"><h2>" + id + " - "+line.name+"</h2><ul id=\"arret_ligne\"></ul></li>"
-            }
-            
-            // On affiche les arrets quand on clique sur la ligne
-
-            const listItems = document.querySelectorAll('#liste li');
-
-            
-            listItems.forEach(item => {
-                item.addEventListener('click', function(event){
-                    const previousActive = document.querySelector('.active');
-
-                    // On vérifie si la ligne a deja les arrets d'afficher
-
-
-                    if (!event.currentTarget.classList.contains('active')) {
-                        
-                        if (previousActive) {
-                            previousActive.classList.remove('active');
-                        }
-                        // Si non on ajoute "active" a sa classe et on affiche les arrets
-                        
-                        const id = event.currentTarget.id;
-                        const stops = new XMLHttpRequest();
-                        stops.open("get", "https://api.tisseo.fr/v2/stop_points.json?key=a3732a1074e2403ce364ad6e71eb998cb&lineId="+id)
-                        stops.onreadystatechange = function () {
-                            if (stops.readyState === 4 && stops.status === 200){
-                                let stop = JSON.parse(stops.responseText)
-                                let arrets = stop.physicalStops.physicalStop
-
-                                let previousStops = [];
-
-                                // Permet d'enelever les arrets doublons
-
-                                for (let i = 0; i < arrets.length; i++) {
-                                  let currentStop = arrets[i].name;
-                                  if (!previousStops.includes(currentStop)) {
-                                    previousStops.push(currentStop);
-                                    // Ajouter l'arrêt actuel à la liste HTML
-                                  };
-                                };
-
-
-
-                                let codeHTML = "";
-                                for (let i = 0; i < previousStops.length; i++){
-
-                                    codeHTML+="<li class='arret'>"+previousStops[i]+"</li>"
-
-                                };
-
-                                // supprime les arrets affichés dans les autres lignes pour éviter de les avoir toutes en meme temps
-
-                                document.querySelectorAll('#liste li ul').forEach(e => e.innerHTML = ""); 
-                                
-                                // affiche les arrets
-
-                                document.querySelector(`#liste li[id="${id}"] #arret_ligne`).innerHTML = codeHTML;
-
-                            }
-                        
-                        }
-                        stops.send();
-                        event.currentTarget.classList.add('active');
-
-                    
-
-                    } else {
-                        
-                        // Si oui on on enlève la classe "active" et on retire les arrets
-
-                        const id = event.currentTarget.id;
-                        document.querySelector(`#liste li[id="${id}"] #arret_ligne`).innerHTML = "";
-                        previousActive.classList.remove('active');
-                    }
-
-      
-                });
-            });
-            
-
-
-        }
-    };
-    xhr.send();
+// Récupère les lignes
+function fetchLines() {
+  const xhr = new XMLHttpRequest();
+  xhr.open("get", "https://api.tisseo.fr/v2/lines.json?key=a3732a1074e2403ce364ad6e71eb998cb");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      let lines = JSON.parse(xhr.responseText).lines.line;
+      displayLines(lines);
+    }
+  };
+  xhr.send();
 }
-);
- 
 
+// Affiche les Lignes
 
+function displayLines(lines) {
+  document.querySelector("#liste").innerHTML = "";
+  lines.sort(function(a, b) {
+    return parseInt(a.id.split(":")[1]) - parseInt(b.id.split(":")[1]);
+  });
+  lines.forEach(line => {
+    let id = line.id.replace("line:", "");
+    let item = `<li class='ligne' id="${line.id}"><h2>${id} - ${line.name}</h2><ul id="arret_ligne"></ul></li>`;
+    document.querySelector("#liste").innerHTML += item;
+  });
+  const listItems = document.querySelectorAll('#liste li');
+  listItems.forEach(item => {
+    item.addEventListener('click', function(event){
+      toggleLine(event.currentTarget);
+    });
+  });
+}
 
+// Selectionne une ligne (Affiche les arrets quand on clique sur les lignes)
+function toggleLine(line) {
+  const previousActive = document.querySelector('.active');
+  if (!line.classList.contains('active')) {
+    if (previousActive) {
+      previousActive.classList.remove('active');
+    }
+    fetchStops(line.id);
+    line.classList.add('active');
+    console.log(line.id)
 
+  } else {
+    document.querySelector(`#liste li[id="${line.id}"] #arret_ligne`).innerHTML = "";
+    previousActive.classList.remove('active');
+  }
+}
 
+// Récupère les arrets
 
+function fetchStops(lineId) {
+  const stops = new XMLHttpRequest();
+  stops.open("get", "https://api.tisseo.fr/v2/stop_points.json?key=a3732a1074e2403ce364ad6e71eb998cb&lineId="+lineId)
+  stops.onreadystatechange = function () {
+    if (stops.readyState === 4 && stops.status === 200){
+      let stop = JSON.parse(stops.responseText).physicalStops.physicalStop;
+      let previousStops = stop.map(s => s.name).filter((v, i, a) => a.indexOf(v) === i);
+      displayStops(lineId, previousStops);
+      console.log("ça marche")
+    }
+  };
+  stops.send()
+};
+
+// Affiche les arrets
+  
+function displayStops(lineId, previousStops) {
+    let codeHTML = "";
+    for (let i = 0; i < previousStops.length; i++){
+        codeHTML+="<li class='arret'>"+previousStops[i]+"</li>"
+    };
+    document.querySelector(`#liste li[id="${lineId}"] #arret_ligne`).innerHTML = codeHTML;
+}
+  
